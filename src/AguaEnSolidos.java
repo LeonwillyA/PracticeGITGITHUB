64943e453541bcc37b1c7dca28588af326eda67b
LeonwillyA <leonalvaradowilly@gmail.com>
2023-11-28 21:48:17 -0500

    el inicio para poder hallar los valores de la humedad relativa de la hoja de tabaco

---
+++ src/AguaEnSolidos.java
@@ -0,0 +1,24 @@
+public class AguaEnSolidos {
+    public static void main(String[] args) {
+        double x = 0.049;
+
+
+          if (x>0 && x<0.032) {
+              System.out.println(
+                      "El valor de la humedad relativa de la hoja del tabajo es: " + x * 33 / 32);
+          }
+          else if (x>=0.032 && x<0.050) {
+              System.out.println(
+                      "El valor de la humedad relativa de la hoja del tabajo es: " + x * 33 / 18);
+          }
+            else {
+              System.out.println("Me falta infor");
+          }
+        }
+
+    }
+
+
+
+
+
